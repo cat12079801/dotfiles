fe76cf5577efa10c1700e04510d57e16da701d7d
cat <cat12079801@gmail.com>
2015-07-11 01:10:40 +0900

    add "tmux" and "tmux-powerline" conf

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zshenv ~/.zshenv
 ln -sf ~/dotfiles/zprofile ~/.zprofile
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 cd
 source ~/.zprofile
 source ~/.zshenv
