9b319935584be6d5cc7c3ad05b218d0be25b096d
cat <cat@localhost.localdomain>
2018-03-21 22:23:46 +0900

    installスクリプトを再度実行できるように

--- install-zsh.sh
+++ install-zsh.sh
@@ -5,7 +5,9 @@ mkdir -p .vim/bundle
 mkdir -p .vim/vimdots
 mkdir -p .vim/backup
 cd ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim.git
+if [ ! -e ~/.vim/bundle/neobundle.vim/ ]; then
+  git clone https://github.com/Shougo/neobundle.vim.git
+fi
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zshenv ~/.zshenv
