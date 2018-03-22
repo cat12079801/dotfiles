f5c8a3236c5728683660e72323796890eb62d463
cat <cat@localhost.localdomain>
2018-03-22 09:27:15 +0900

    install scriptからneobundleを削除、pyenvを追加

--- install-zsh.sh
+++ install-zsh.sh
@@ -5,15 +5,19 @@ mkdir -p .vim/bundle
 mkdir -p .vim/vimdots
 mkdir -p .vim/backup
 cd ~/.vim/bundle
-if [ ! -e ~/.vim/bundle/neobundle.vim/ ]; then
-  git clone https://github.com/Shougo/neobundle.vim.git
+
+if [ ! -e ~/.pyenv/ ]; then
+  git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+  git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 fi
+
 if [ ! -e ~/.cache/dein ]; then
   mkdir ~/.vim/dein.vim/
   curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein.vim-installer.sh
   sh ./dein.vim-installer.sh ~/.cache/dein
   rm dein.vim-installer.sh
 fi
+
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zshenv ~/.zshenv
