9076ba01aaae0a516eb6a5bb5bcc3aa102abb9a5
cat12079801 <cat12079801@gmail.com>
2018-04-04 17:40:01 +0900

    パスなど修正

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -66,7 +66,7 @@ if &compatible
 endif
 
 " Required:
-set runtimepath+=/home/cat/.cache/dein/repos/github.com/Shougo/dein.vim
+set runtimepath+=~/.cache/dein/repos/github.com/Shougo/dein.vim
 
 " Required:
 if dein#load_state(s:dein_dir)
--- install-zsh.sh
+++ install-zsh.sh
@@ -11,8 +11,12 @@ if [ ! -e ~/.pyenv/ ]; then
   git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 fi
 
+mkdir -p ~/.cache/dein/repos/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git \
+      ~/.vim/dein/repos/github.com/Shougo/dein.vim
+
 if [ ! -e ~/.cache/dein ]; then
-  mkdir ~/.vim/dein.vim/
+  mkdir -p ~/.vim/dein.vim/
   curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein.vim-installer.sh
   sh ./dein.vim-installer.sh ~/.cache/dein
   rm dein.vim-installer.sh
@@ -24,7 +28,8 @@ ln -sf ~/dotfiles/zshenv ~/.zshenv
 ln -sf ~/dotfiles/zprofile ~/.zprofile
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/config ~/.config
+mkdir -p ~/.config/
+ln -sf ~/dotfiles/config/nvim ~/.config/nvim
 cd
 source ~/.zprofile
 source ~/.zshenv
