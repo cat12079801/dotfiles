04787a322467273bd73a52e6c7b0b4687958ba85
cat12079801 <cat12079801@gmail.com>
2026-01-29 10:17:54 +0900

    chore: update tmux conf

--- tmux/ip.sh
+++ tmux/ip.sh
@@ -1,10 +1,2 @@
 # How do I get the interface name?
-if type ifdata >/dev/null 2>&1; then
-  if ifdata -pa eth0 >/dev/null 2>&1; then
-    ifdata -pa eth0
-  elif ifdata -pa en0 >/dev/null 2>&1; then
-    ifdata -pa en0
-  fi
-else
-  echo "no ifdata cmd"
-fi
+ipconfig getifaddr en0 2>/dev/null || echo "N/A"
